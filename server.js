4c97e27617243567f4605e1e354483bb94c084ed
Ash Grey <111319560+ashxtt@users.noreply.github.com>
2022-10-19 22:42:02 -0700

    Update server.js

--- server.js
+++ server.js
@@ -6,16 +6,16 @@ const port = 8000
 const responses = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
 
 //!Greetings
-app.get('/greeting', (req, res) =>{
+app.get('/greeting', (req, res) => {
     res.send('Hello,stranger')
 })
 
-app.get('/greeting/:name', (req, res) =>{
+app.get('/greeting/:name', (req, res) => {
     res.send(`Hello! ${(req.params.name)}`)
 })
 
 //!Tip Calculator
-app.get("/tip/:total/:tipPercentage", (req,res)=> {
+app.get("/tip/:total/:tipPercentage", (req,res) => {
     res.send(`${parseInt(req.params.total) * parseInt(req.params.tipPercentage)/100}`)
 })
 
@@ -24,11 +24,11 @@ app.get("/magic/:question", (req,res) => {
     res.send(`<h1>${req.params.question} = ${responses[Math.floor(Math.random() *responses.length)]}</h1>`)
 })
  //!Take one down pass it around
- app.get("/", (req, res)=>{
+ app.get("/", (req, res) => {
     res.send("99 Bottles of beer on the wall")
  })
 
- app.get("/:number_of_bottles", (req, res)=>{
+ app.get("/:number_of_bottles", (req, res) => {
     res.render()
  })
 
